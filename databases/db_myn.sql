9cd5f09cb94ad74dec5b1d7cad9085c59fe22096
aldhyrahim10 <frasastyan10@gmail.com>
2021-06-05 05:12:00 +0700

    Update database terbaru

--- databases/db_myn.sql
+++ databases/db_myn.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Waktu pembuatan: 01 Jun 2021 pada 13.22
--- Versi server: 10.4.17-MariaDB
--- Versi PHP: 7.3.27
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 05 Jun 2021 pada 00.11
+-- Versi server: 10.4.18-MariaDB
+-- Versi PHP: 7.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,15 +33,20 @@ CREATE TABLE `catalog` (
   `deskripsi_catalog` text NOT NULL,
   `id_designer` int(11) NOT NULL,
   `id_category` int(11) NOT NULL,
-  `image_master` varchar(128) NOT NULL
+  `image_master` varchar(128) NOT NULL,
+  `img_supp1` varchar(128) NOT NULL,
+  `img_supp2` varchar(128) NOT NULL,
+  `img_supp3` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `catalog`
 --
 
-INSERT INTO `catalog` (`id_catalog`, `nama_catalog`, `deskripsi_catalog`, `id_designer`, `id_category`, `image_master`) VALUES
-(1, 'Design Minimalist', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 1, 1, '');
+INSERT INTO `catalog` (`id_catalog`, `nama_catalog`, `deskripsi_catalog`, `id_designer`, `id_category`, `image_master`, `img_supp1`, `img_supp2`, `img_supp3`) VALUES
+(4, 'Kitchen Minimalis', 'deskripsi', 2, 1, 'image25.png', '', '', ''),
+(5, 'Bedroom Minimalist', 'Bedroom Minimalist', 2, 10, 'catalogchild11.png', '', '', ''),
+(8, 'Living Room Set', 'Living Room Set', 2, 11, 'catalog19.png', 'image35.png', 'catalogchild33.png', 'catalogchild21.png');
 
 -- --------------------------------------------------------
 
@@ -75,6 +80,7 @@ CREATE TABLE `designer` (
   `email_designer` varchar(128) NOT NULL,
   `telp_designer` varchar(15) NOT NULL,
   `alamat_designer` text NOT NULL,
+  `profile` varchar(128) NOT NULL,
   `maps` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -82,23 +88,32 @@ CREATE TABLE `designer` (
 -- Dumping data untuk tabel `designer`
 --
 
-INSERT INTO `designer` (`id_designer`, `nama_designer`, `email_designer`, `telp_designer`, `alamat_designer`, `maps`) VALUES
-(1, 'Miskah', '', '089976823231', 'Jl. Mawar Melati No. 32 Semarang', 'https://maps.google.com/'),
-(2, 'Miskah Alfiyyah Kulsum', 'miskah@gmail.com', '087788832424', 'jl. Merdeka 3 No. 32 Bandung', ''),
-(3, 'Muhammad Valensyah', 'valen@gmail.com', '0812234334556', 'jl. Pahlawan Raya No. 40 Bandung', ''),
-(4, 'Yoga Pranata', '', '0899424244542', 'Bandung', '');
+INSERT INTO `designer` (`id_designer`, `nama_designer`, `email_designer`, `telp_designer`, `alamat_designer`, `profile`, `maps`) VALUES
+(1, 'Miskah', '', '089976823231', 'Jl. Mawar Melati No. 32 Semarang', 'default.jpg', 'https://maps.google.com/'),
+(2, 'Miskah Alfiyyah Kulsum', 'miskah@gmail.com', '6282678563492', 'jl. Merdeka 3 No. 32 Bandung', 'default.jpg', ''),
+(3, 'Muhammad Valensyah', 'valen@gmail.com', '62812234334556', 'jl. Pahlawan Raya No. 40 Bandung', 'default.jpg', ''),
+(4, 'Yoga Pranata', 'yoga@gmail.com', '62899424244542', 'Bandung', 'default.jpg', '');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `images`
+-- Struktur dari tabel `feedback`
 --
 
-CREATE TABLE `images` (
-  `id_image` int(11) NOT NULL,
+CREATE TABLE `feedback` (
+  `id_feedback` int(11) NOT NULL,
   `id_catalog` int(11) NOT NULL,
-  `nama_image` varchar(128) NOT NULL
+  `nama` varchar(128) NOT NULL,
+  `review` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `feedback`
+--
+
+INSERT INTO `feedback` (`id_feedback`, `id_catalog`, `nama`, `review`) VALUES
+(3, 8, 'nita', 'sangat bagus'),
+(5, 8, 'nita', 'sangat bagus');
 
 -- --------------------------------------------------------
 
@@ -110,6 +125,7 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `nama` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
+  `telp_user` varchar(20) NOT NULL,
   `password` varchar(128) NOT NULL,
   `role` int(1) NOT NULL COMMENT '1=admin\r\n2=designer',
   `status` int(1) NOT NULL COMMENT '0=not active\r\n1 = active',
@@ -120,11 +136,11 @@ CREATE TABLE `user` (
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`id`, `nama`, `email`, `password`, `role`, `status`, `avatar_user`) VALUES
-(1, 'admin', 'admin@gmail.com', '0192023a7bbd73250516f069df18b500', 1, 1, 'default.jpg'),
-(2, 'Miskah Alfiyyah Kulsum', 'miskah@gmail.com', '3b3aa58a4a922f81c38fb8bb7e2a41a0', 2, 1, 'default.jpg'),
-(3, 'Muhammad Valensyah', 'valen@gmail.com', 'eed59ea2337108e10d5dced609999557', 2, 0, 'default.jpg'),
-(4, 'Yoga Pranata', 'yoga@gmail.com', '3265c569f3d1f00b371e90080d6ab5dd', 2, 1, 'default.jpg');
+INSERT INTO `user` (`id`, `nama`, `email`, `telp_user`, `password`, `role`, `status`, `avatar_user`) VALUES
+(1, 'admin', 'admin@gmail.com', '', '0192023a7bbd73250516f069df18b500', 1, 1, 'default.jpg'),
+(2, 'Miskah Alfiyyah Kulsum', 'miskah@gmail.com', '6282678563492', '25d55ad283aa400af464c76d713c07ad', 2, 1, 'default.jpg'),
+(3, 'Muhammad Valensyah', 'valen@gmail.com', '62812234334556', 'eed59ea2337108e10d5dced609999557', 2, 1, 'default.jpg'),
+(4, 'Yoga Pranata', 'yoga@gmail.com', '62899424244542', '3265c569f3d1f00b371e90080d6ab5dd', 2, 1, 'default.jpg');
 
 --
 -- Indexes for dumped tables
@@ -134,9 +150,7 @@ INSERT INTO `user` (`id`, `nama`, `email`, `password`, `role`, `status`, `avatar
 -- Indeks untuk tabel `catalog`
 --
 ALTER TABLE `catalog`
-  ADD PRIMARY KEY (`id_catalog`),
-  ADD UNIQUE KEY `id_designer` (`id_designer`),
-  ADD UNIQUE KEY `id_category` (`id_category`);
+  ADD PRIMARY KEY (`id_catalog`);
 
 --
 -- Indeks untuk tabel `category`
@@ -151,11 +165,10 @@ ALTER TABLE `designer`
   ADD PRIMARY KEY (`id_designer`);
 
 --
--- Indeks untuk tabel `images`
+-- Indeks untuk tabel `feedback`
 --
-ALTER TABLE `images`
-  ADD PRIMARY KEY (`id_image`),
-  ADD UNIQUE KEY `id_catalog` (`id_catalog`);
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id_feedback`);
 
 --
 -- Indeks untuk tabel `user`
@@ -171,7 +184,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `catalog`
 --
 ALTER TABLE `catalog`
-  MODIFY `id_catalog` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_catalog` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `category`
@@ -186,10 +199,10 @@ ALTER TABLE `designer`
   MODIFY `id_designer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `images`
+-- AUTO_INCREMENT untuk tabel `feedback`
 --
-ALTER TABLE `images`
-  MODIFY `id_image` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `feedback`
+  MODIFY `id_feedback` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
@@ -207,12 +220,6 @@ ALTER TABLE `user`
 ALTER TABLE `catalog`
   ADD CONSTRAINT `catalog_ibfk_1` FOREIGN KEY (`id_designer`) REFERENCES `designer` (`id_designer`),
   ADD CONSTRAINT `catalog_ibfk_2` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`);
-
---
--- Ketidakleluasaan untuk tabel `images`
---
-ALTER TABLE `images`
-  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`id_catalog`) REFERENCES `catalog` (`id_catalog`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
